2a00db13330997865af80417e55791e24f2c0dc3
yintagoka <yintagoka@gmail.com>
2017-12-13 22:33:38 +1300

    ball animated
    balls move and bouncing against the wall

--- js/ball.js
+++ js/ball.js
@@ -18,29 +18,46 @@ function generateBalls(numberOfBall) {
   ballArray = [];
   for (var i = 0; i < numberOfBall; i++) {
   	var ball = {
-  	  x:width/2,
-  	  y:height/2,
-  	  color:colorArray[Math.round(Math.random() * (colorArray.length - 1))],
-  	  radius:10 + Math.round(Math.random()*90),
-  	  speedX:1+Math.round(Math.random()*9),
-  	  speedY:1+Math.round(Math.random()*9),
+  	  x:width/2,  // start from center
+  	  y:height/2, // start from center
+  	  color:colorArray[Math.round(Math.random() * 100) % colorArray.length], // between 0 to length of colorArray
+  	  radius:10 + Math.round(Math.random() * 100) % 90,  // between 10 and 100
+  	  speedX:-10 + Math.round(Math.random() * 100) % 20, // between -10 and +10
+  	  speedY:-10 + Math.round(Math.random() * 100) % 20, // between -10 and +10
   	}
 
   	ballArray.push(ball);
   	drawCircle(ball);
   }
+  console.log('ballArray.length = ' + ballArray.length);
 }
 
 function animate() {
-	canvas.clearRect(0,0,width,height);
+  console.log('animate');
+	ctx.clearRect(0,0,width,height);
 	moveBall();
+  bounceWhenHitWall();
 	requestAnimationFrame(animate);
 }
 
-function moveBall(ballArray) {
+function moveBall() {
   for (var i=0, max = ballArray.length; i < max; i++) {
   	var ball = ballArray[i];
+    ball.x += ball.speedX;
+    ball.y += ball.speedY;
+    drawCircle(ball);
+  }
+}
 
+function bounceWhenHitWall() {
+  for (var i = 0, max = ballArray.length; i < max; i++) {
+    var ball = ballArray[i];
+    if (ball.x + ball.radius >= width || ball.x - ball.radius <= 0) {
+      ball.speedX *= -1;
+    }
+    if (ball.y + ball.radius >= height || ball.y - ball.radius <= 0) {
+      ball.speedY *= -1;
+    }
   }
 }
 
@@ -53,8 +70,6 @@ function drawCircle(ball) {
 
   ctx.arc(0, 0, ball.radius, 0, 2*Math.PI);
   ctx.fill();
-  ctx.fillStyle = 'green';
-  ctx.fillRect(0,0,30,30);
 
   ctx.restore();
 }
